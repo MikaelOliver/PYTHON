0a4088963af471963170f186f0f9cc8697f9868e
Mikael Oliveira <mikaeloliver0987@gmail.com>
2023-10-17 16:28:21 -0300

    Comentarios adicionados no arq duas_strings.py

--- aula10.17/duas_strings.py
+++ aula10.17/duas_strings.py
@@ -1,3 +1,6 @@
+# texto1 recebe o valor "Sou do"
 texto1 = "Sou do"
+# texto2 recebe o valor "back-end"
 texto2 = "beck-end"
+# a função print, contatena e imprime as duas variaveis na tela
 print(texto1,texto2)
