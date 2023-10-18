f1c5d79d2db77e1f604b544c5f4a82893abd47c7
Mikael Oliveira <mikaeloliver0987@gmail.com>
2023-10-18 16:53:30 -0300

    adicionando a div que tinha esquecido na questão

--- aula10.18/desafio07.py
+++ aula10.18/desafio07.py
@@ -2,7 +2,7 @@ num01 = input("Digite o primeiro numero: ")
 num02 = input("Digite o segundo numero: ")
 num03 = input("Digte o terceiro numero: ")
 
-Calculo = int(num01) *2 + int(num02) + int(num03)
+Calculo = int(num01) *2 + int(num02)/2 + int(num03)
 Calculo02 = int(num01) *3 + int(num03) * int(num02)
 
 print(Calculo)
