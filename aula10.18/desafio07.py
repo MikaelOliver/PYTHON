4c44158f67533e3367f007112250eed4473663ce
Mikael Oliveira <mikaeloliver0987@gmail.com>
2023-10-18 17:00:15 -0300

    Ajustando os calculos

--- aula10.18/desafio07.py
+++ aula10.18/desafio07.py
@@ -2,8 +2,8 @@ num01 = input("Digite o primeiro numero: ")
 num02 = input("Digite o segundo numero: ")
 num03 = input("Digte o terceiro numero: ")
 
-Calculo = int(num01) *2 + int(num02)/2 + int(num03)
-Calculo02 = int(num01) *3 + int(num03) * int(num02)
+Calculo = (int(num01) *2 + int(num02)/2) + int(num03)
+Calculo02 = (int(num01) *3 + int(num03)) * int(num02)
 
 print(Calculo)
 print(Calculo02)
