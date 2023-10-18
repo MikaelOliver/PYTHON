10ca72f12bcd05ffd612937e517ec4215f958aae
Mikael Oliveira <mikaeloliver0987@gmail.com>
2023-10-18 16:50:18 -0300

    arquivos da aula10.18 foram adicionados

---
+++ aula10.18/desafio07.py
@@ -0,0 +1,10 @@
+num01 = input("Digite o primeiro numero: ")
+num02 = input("Digite o segundo numero: ")
+num03 = input("Digte o terceiro numero: ")
+
+Calculo = int(num01) *2 + int(num02) + int(num03)
+Calculo02 = int(num01) *3 + int(num03) * int(num02)
+
+print(Calculo)
+print(Calculo02)
+
