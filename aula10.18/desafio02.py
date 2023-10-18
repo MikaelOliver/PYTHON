26a0f96571f18d1b3310f62eb07e8cacd8d09719
Mikael Oliveira <mikaeloliver0987@gmail.com>
2023-10-18 15:59:59 -0300

    Criado desafio02.py e usando o input

---
+++ aula10.18/desafio02.py
@@ -0,0 +1,4 @@
+idade = input("informe a sua idade:")
+idade_parente = input("Informe a idade de algum parente:")
+nome_usuario = input("Infrme o seu nome:")
+print(int(idade) * int(idade_parente), nome_usuario )
