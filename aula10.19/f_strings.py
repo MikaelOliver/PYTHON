2ece94fdd70ed20c6953ad3c8df68096c3817582
Mikael Oliveira <mikaeloliver0987@gmail.com>
2023-10-19 15:10:39 -0300

    Aprendando intepolação

---
+++ aula10.19/f_strings.py
@@ -0,0 +1,8 @@
+# INTERPOANDO STRINGS COM CODIGO PYTHO 
+
+nome = input("qual o seu nome? ")
+#concatenação
+print("Seja muito bem-vindo", nome)
+
+# interpolação
+print(f'Seja muito bem-vindo{nome}. Estavamos esperando por você!!')
