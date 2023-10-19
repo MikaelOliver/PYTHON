3f9a07a578e14267484eebeb3c9dc0fcaf24da6a
Mikael Oliveira <mikaeloliver0987@gmail.com>
2023-10-19 14:18:37 -0300

    Desafio07.py modificado

--- aula10.18/desafio07.py
+++ aula10.18/desafio07.py
@@ -2,6 +2,6 @@ num01 = input("Digite o primeiro numero: ")
 num02 = input("Digite o segundo numero: ")
 num03 = input("Digte o terceiro numero: ")
 
-print((int(num01) *2) + (int(num02)/2) + int(num03))
+print((int(num01) *2) + int(num02)/2 + int(num03))
 print((int(num01) *3 + int(num03)) * int(num02))
 
