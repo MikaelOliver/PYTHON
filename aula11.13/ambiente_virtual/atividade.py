2cea99ba6dd508860670a9ea776e3134cd304d20
Mikael Oliveira <mikaeloliver0987@gmail.com>
2023-11-13 16:41:53 -0300

    Atividades do dia 13 adicionadas, conteudo de listas

---
+++ aula11.13/ambiente_virtual/atividade.py
@@ -0,0 +1,7 @@
+lista = ['jeff', 'kaynan', 'felipe', 'sedex','artur']
+print(lista)
+lista.append('Paulo')
+del lista[2]
+lista.remove('sedex')
+print(lista)
+lista.append('Gabriel')
---
+++ aula11.13/ambiente_virtual/listas.py
@@ -0,0 +1,40 @@
+# uma lista é representada pelos []
+# len - metodo que retorna a quantidade de itens de uma lista - OBS: todo metodo, por obrigação retorna algum valor.
+# append -ele coloca os valores no fim do lista
+#del - remove item especifico da lista
+#remove - remove um objeto especifico da lista
+#pop - remove o ultimo objeto da lista
+#insert - adiciona um objeto em qualquer lugar da lista
+lista = []
+lista = ['back']
+lista.append('front') 
+print(lista, type(lista))
+print(len(lista))
+lista.append('data')
+print(lista, type(lista))
+
+lista = ['back','tarde',21, True, 8.8]
+print(f'a quantidade de alunos na turma é:{lista[2]}')
+
+lista[2] = 22
+print(lista)
+
+lista[1] = False
+print(lista)
+
+lista[1] = ['neyva','alice','lara','paula', 'geisa']
+print(lista)
+print(lista[1][2])
+
+print(lista[-2])
+del lista[-2]
+print(lista[-2])
+print(lista)
+
+lista.remove('back')
+print(lista)
+
+lista.insert(0,'Amontada Valley')
+print(lista)
+lista.insert(2,'professor')
+print(lista)
