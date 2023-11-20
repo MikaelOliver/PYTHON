7af7c128bae137f79d64ce0ad727d3663d90278a
Mikael Oliveira <mikaeloliver0987@gmail.com>
2023-11-20 13:38:54 -0300

    Adiviacao,py terminado

--- CURSOS/adiviacao.py
+++ CURSOS/adiviacao.py
@@ -1,18 +1,29 @@
+import random
+
 print('='*32)
 print('Bem vindo ao jogo de adiviação!')
 print('='*32)
 
-num_scret = 42
+num_scret= round(random.random() * 100)
+total_tentativas = 10
 
-chute = int(input('Digite o seu número: '))
-print(f'O seu numero é:{chute}')
-if(num_scret==chute):
-    print('você acertou!!')
-else:
-    if(num_scret < chute):
-        print('Você fez um chute maior do que o numero secreto')
-    if(num_scret > chute):
-        print('Você fez um chute menor do que o numero secreto')
+for rodadas in range(1, total_tentativas +1):
+    print(f'Tentativas {rodadas} de {total_tentativas}')
+    chute = int(input('Digite um número entre 1 e 100: '))
+   
+    print(f'O seu numero é:{chute}')
+    if(chute< 1 or chute>100):
+        print("Você deve digitar um número entre 1 e 100")
+        continue
+    if(num_scret==chute):
+        print('você acertou!!')
+        break
+    else:
+        if(num_scret < chute):
+            print('Você fez um chute maior do que o numero secreto')
+        elif(num_scret >   chute):
+            print('Você fez um chute menor do que o numero secreto')
+print(F'O número secreto é:{num_scret}')
 print('='*30)
 print('Fim do jogo!')
 print('='*30)
