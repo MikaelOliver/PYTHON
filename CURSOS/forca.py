44ebe75a8624d31a9ade456652d7a23a42fd5521
Mikael Oliveira <mikaeloliver0987@gmail.com>
2023-11-21 20:20:28 -0300

    forca alterado

--- CURSOS/forca.py
+++ CURSOS/forca.py
@@ -3,6 +3,22 @@ def jogar():
     print('Bem vindo ao jogo de adiviação!')
     print('='*32)
 
+    palavra_secreta = 'banana'
+    enforcou = False
+    acertou = False
+
+
+    while(not enforcou and not acertou):
+
+        chute = input('Qual letra: ')
+        
+        print('jogando...')
+
+
+
+
+
+
 
 if(__name__ == '__main__'):
     jogar()
